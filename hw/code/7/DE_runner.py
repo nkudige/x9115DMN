89fedbb97cefc7d56e3e35fd6b55629c70e30bd0
deepakrn <deepak.nandihalli@gmail.com>
2015-11-15 23:29:59 +0000

    Running DE with elite sampling on all models

--- hw/code/7/DE_runner.py
+++ hw/code/7/DE_runner.py
@@ -1,14 +1,15 @@
 from DE import DE
 
-de = DE("Golinski")
-print "Running DE with 33% elite sampling"
-de.run(s=0.33)
-print "Running DE with 50% elite sampling"
-de = DE("Golinski")
-de.run(s=0.50)
-print "Running DE with 70% elite sampling"
-de = DE("Golinski")
-de.run(s=0.70)
-print "Running DE with 100% elite sampling"
-de = DE("Golinski")
-de.run(s=1)
+for model in ["Golinski", "Schaffer", "Kursawe", "Osyczka"]:
+    de = DE(model)
+    print "Running DE for " + model + "with 33% elite sampling"
+    de.run(s=0.33)
+    print "Running DE for " + model + "with 50% elite sampling"
+    de = DE(model)
+    de.run(s=0.50)
+    print "Running DE for " + model + "with 70% elite sampling"
+    de = DE(model)
+    de.run(s=0.70)
+    print "Running DE for " + model + "with 100% elite sampling"
+    de = DE(model)
+    de.run(s=1)
---
+++ hw/code/7/README.md
@@ -0,0 +1,19 @@
+# x9115KMN
+# CSC 591 - Automated Software Engineering
+
+## Programming Assignment 6
+* Differential Evolution (DE.py) is run for different models and the medians of the frontier are printed.
+
+DE is run on the following models:
+* Schaffer (Schaffer.py)
+* Kursawe (Kursawe.py)
+* Golinski (Golinski.py)
+* Osyczka (Osyczka.py)
+
+... that can be executed as follows:
+* python DE_runner.py
+
+* The median values in the frontier for different sampling(33%, 50%, 70%, 100%) rates indicate that the sampling rate doesn't affect 
+the median of results of the state energies in the frontier. Thus when the algorithm is run with lower value of s, it takes lesser time
+to execute. This means that DE(s) is would be useful in optimizing the search for best state.
+
